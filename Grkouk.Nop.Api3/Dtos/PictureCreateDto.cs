d3b00e0a624110d0113016f9a27f615d3f65b143
GrKouk <grkouk@hotmail.com>
2020-01-17 18:36:38 +0200

    PictureCreateDto

---
+++ Grkouk.Nop.Api3/Dtos/PictureCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grkouk.Nop.Api3.Dtos
+{
+    public class PictureCreateDto   
+    {
+     
+        public string MimeType { get; set; }
+        public string SeoFilename { get; set; }
+        public string AltAttribute { get; set; }
+        public string TitleAttribute { get; set; }
+    }
+}
